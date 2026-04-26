d1769ac0b109c867de344a59a9405a8cc6ac0e81
uommou <ffffff.990917@gmail.com>
2026-04-26 17:39:22 +0900

    style

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Chaerryos Blog",
+    pageTitle: "chaerryos-blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
