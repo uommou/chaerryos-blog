4e535e3e131ef2eba1568a199f123abb48e9e9a6
uommou <ffffff.990917@gmail.com>
2026-04-26 17:07:24 +0900

    chore: recent notes style

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -42,37 +42,34 @@ export default ((userOpts?: Partial<Options>) => {
           {pages.slice(0, opts.limit).map((page) => {
             const title = page.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title
             const tags = page.frontmatter?.tags ?? []
+            const date = getDate(cfg, page)
 
             return (
               <li class="recent-li">
-                <div class="section">
-                  <div class="desc">
-                    <h3>
-                      <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
-                        {title}
-                      </a>
-                    </h3>
-                  </div>
-                  {page.dates && (
-                    <p class="meta">
-                      <Date date={getDate(cfg, page)!} locale={cfg.locale} />
-                    </p>
-                  )}
-                  {opts.showTags && (
-                    <ul class="tags">
-                      {tags.map((tag) => (
-                        <li>
-                          <a
-                            class="internal tag-link"
-                            href={resolveRelative(fileData.slug!, `tags/${tag}` as FullSlug)}
-                          >
-                            {tag}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
+                <div class="recent-row">
+                  <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal recent-title">
+                    {title}
+                  </a>
+                  {date && (
+                    <span class="recent-date">
+                      <Date date={date} locale={cfg.locale} />
+                    </span>
                   )}
                 </div>
+                {opts.showTags && tags.length > 0 && (
+                  <ul class="recent-tags">
+                    {tags.map((tag) => (
+                      <li>
+                        <a
+                          class="internal tag-link"
+                          href={resolveRelative(fileData.slug!, `tags/${tag}` as FullSlug)}
+                        >
+                          {tag}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </li>
             )
           })}
