e361e2fc334e9d281f0e1676afd4f7db34ffaa2a
uommou <ffffff.990917@gmail.com>
2026-05-11 02:54:39 +0900

    style: sub category

--- quartz/components/pages/FolderContent.tsx
+++ quartz/components/pages/FolderContent.tsx
@@ -1,7 +1,7 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 import style from "../styles/listPage.scss"
-import { PageList, SortFn } from "../PageList"
+import { PageList, SortFn, byDateAndAlphabetical } from "../PageList"
 import { Root } from "hast"
 import { htmlToJsx } from "../../util/jsx"
 import { i18n } from "../../i18n"
@@ -9,6 +9,8 @@ import { QuartzPluginData } from "../../plugins/vfile"
 import { ComponentChildren } from "preact"
 import { concatenateResources } from "../../util/resources"
 import { trieFromAllFiles } from "../../util/ctx"
+import { FullSlug, isFolderPath, resolveRelative } from "../../util/path"
+import { Date as DateComponent, getDate } from "../Date"
 
 interface FolderContentOptions {
   /**
@@ -88,19 +90,40 @@ export default ((opts?: Partial<FolderContentOptions>) => {
           }
         })
         .filter((page) => page !== undefined) ?? []
+
     const cssClasses: string[] = fileData.frontmatter?.cssclasses ?? []
     const classes = cssClasses.join(" ")
-    const listProps = {
-      ...props,
-      sort: options.sort,
-      allFiles: allPagesInFolder,
-    }
 
     const content = (
       (tree as Root).children.length === 0
         ? fileData.description
         : htmlToJsx(fileData.filePath!, tree)
     ) as ComponentChildren
+
+    // Separate subfolders from posts
+    const subfolderPages = allPagesInFolder.filter((page) => isFolderPath(page.slug ?? ""))
+    const postPages = allPagesInFolder
+      .filter((page) => !isFolderPath(page.slug ?? ""))
+      .sort(byDateAndAlphabetical(cfg))
+
+    // Group posts by year → month (both descending)
+    const groupedByYear = new Map<number, Map<number, QuartzPluginData[]>>()
+    const undatedPosts: QuartzPluginData[] = []
+    for (const post of postPages) {
+      const date = getDate(cfg, post)
+      if (date) {
+        const year = date.getFullYear()
+        const month = date.getMonth()
+        if (!groupedByYear.has(year)) groupedByYear.set(year, new Map())
+        if (!groupedByYear.get(year)!.has(month)) groupedByYear.get(year)!.set(month, [])
+        groupedByYear.get(year)!.get(month)!.push(post)
+      } else {
+        undatedPosts.push(post)
+      }
+    }
+    const sortedYears = [...groupedByYear.keys()].sort((a, b) => b - a)
+
+    const subfolderListProps = { ...props, sort: options.sort, allFiles: subfolderPages }
 
     return (
       <div class="popover-hint">
@@ -114,7 +137,71 @@ export default ((opts?: Partial<FolderContentOptions>) => {
             </p>
           )}
           <div>
-            <PageList {...listProps} />
+            {subfolderPages.length > 0 && <PageList {...subfolderListProps} />}
+            {sortedYears.map((year) => {
+              const monthMap = groupedByYear.get(year)!
+              const sortedMonths = [...monthMap.keys()].sort((a, b) => b - a)
+              return (
+                <div class="year-group">
+                  <h3 class="year-header">{year}</h3>
+                  {sortedMonths.map((month) => {
+                    const monthPosts = monthMap.get(month)!
+                    const monthLabel = new Intl.DateTimeFormat(cfg.locale, {
+                      month: "long",
+                    }).format(new Date(year, month))
+                    return (
+                      <div class="month-group">
+                        <h4 class="month-header">{monthLabel}</h4>
+                        <ul class="section-ul">
+                          {monthPosts.map((page) => {
+                            const title = page.frontmatter?.title
+                            const tags = page.frontmatter?.tags ?? []
+                            const date = getDate(cfg, page)
+                            return (
+                              <li class="section-li">
+                                <div class="section-row">
+                                  <a
+                                    href={resolveRelative(fileData.slug!, page.slug!)}
+                                    class="internal section-title"
+                                  >
+                                    {title}
+                                  </a>
+                                  {date && (
+                                    <span class="section-date">
+                                      <DateComponent date={date} locale={cfg.locale} />
+                                    </span>
+                                  )}
+                                </div>
+                                {tags.length > 0 && (
+                                  <ul class="tags">
+                                    {tags.map((tag) => (
+                                      <li>
+                                        <a
+                                          class="internal tag-link"
+                                          href={resolveRelative(
+                                            fileData.slug!,
+                                            `tags/${tag}` as FullSlug,
+                                          )}
+                                        >
+                                          {tag}
+                                        </a>
+                                      </li>
+                                    ))}
+                                  </ul>
+                                )}
+                              </li>
+                            )
+                          })}
+                        </ul>
+                      </div>
+                    )
+                  })}
+                </div>
+              )
+            })}
+            {undatedPosts.length > 0 && (
+              <PageList {...{ ...props, sort: options.sort, allFiles: undatedPosts }} />
+            )}
           </div>
         </div>
       </div>
